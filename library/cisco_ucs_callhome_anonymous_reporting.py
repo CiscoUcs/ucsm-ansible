6e6d1e3bd35f7c76c139ad2ddfd076462fdeda36
Rahul Gupta <ragupta4@cisco.com>
2017-06-22 12:35:11 +0530

    adds gpl license and quotes version
    Signed-off-by: Rahul Gupta <ragupta4@cisco.com>

--- library/cisco_ucs_callhome_anonymous_reporting.py
+++ library/cisco_ucs_callhome_anonymous_reporting.py
@@ -1,4 +1,20 @@
-#!/usr/bin/env python
+#!/usr/bin/python
+
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
 
 from ansible.module_utils.basic import *
 
@@ -11,7 +27,7 @@ DOCUMENTATION = '''
 ---
 module: cisco_ucs_callhome_anonymous_reporting
 short_description: configures callhome anonymous reporting on cisco ucs server
-version_added: 0.9.0.0
+version_added: "0.9.0.0"
 description:
    -  configures callhome anonymous reporting on a cisco ucs server
 options:
