90e8260a848163aff7684e35d05d80edc92e783f
Brett Johnson <brett@sdbrett.com>
2018-07-13 07:01:47 +1000

    Changed to allow changes to existing IP blocks

--- library/ucs_ip_pool.py
+++ library/ucs_ip_pool.py
@@ -250,33 +250,33 @@ def remove_ip_block(ucs, dn, ip_block, ip_version):
 
 
 def update_ip_block(ucs, mo, ip_block, ip_version):
-    if ip_block['state'] == 'absent':
-        remove_ip_block(ucs, mo.dn, ip_block, ip_version)
 
-    elif ip_version == 'v6':
-        from ucsmsdk.mometa.ippool.IppoolIpV6Block import IppoolIpV6Block
-        IppoolIpV6Block(parent_mo_or_dn=mo,
-                        to=ip_block['ipv6_last_addr'],
-                        r_from=ip_block['ipv6_first_addr'],
-                        prefix=ip_block['ipv6_prefix'],
-                        def_gw=ip_block['ipv6_default_gw'],
-                        prim_dns=ip_block['ipv6_primary_dns'],
-                        sec_dns=ip_block['ipv6_secondary_dns']
+    remove_ip_block(ucs, mo.dn, ip_block, ip_version)
+    if not ip_block['state'] == 'absent':
+        if ip_version == 'v6':
+            from ucsmsdk.mometa.ippool.IppoolIpV6Block import IppoolIpV6Block
+            IppoolIpV6Block(parent_mo_or_dn=mo,
+                            to=ip_block['ipv6_last_addr'],
+                            r_from=ip_block['ipv6_first_addr'],
+                            prefix=ip_block['ipv6_prefix'],
+                            def_gw=ip_block['ipv6_default_gw'],
+                            prim_dns=ip_block['ipv6_primary_dns'],
+                            sec_dns=ip_block['ipv6_secondary_dns']
+                            )
+            ucs.login_handle.add_mo(mo, True)
+            ucs.login_handle.commit()
+        else:
+            from ucsmsdk.mometa.ippool.IppoolBlock import IppoolBlock
+            IppoolBlock(parent_mo_or_dn=mo,
+                        to=ip_block['last_addr'],
+                        r_from=ip_block['first_addr'],
+                        subnet=ip_block['subnet_mask'],
+                        def_gw=ip_block['default_gw'],
+                        prim_dns=ip_block['primary_dns'],
+                        sec_dns=ip_block['secondary_dns']
                         )
-        ucs.login_handle.add_mo(mo, True)
-        ucs.login_handle.commit()
-    else:
-        from ucsmsdk.mometa.ippool.IppoolBlock import IppoolBlock
-        IppoolBlock(parent_mo_or_dn=mo,
-                    to=ip_block['last_addr'],
-                    r_from=ip_block['first_addr'],
-                    subnet=ip_block['subnet_mask'],
-                    def_gw=ip_block['default_gw'],
-                    prim_dns=ip_block['primary_dns'],
-                    sec_dns=ip_block['secondary_dns']
-                    )
-        ucs.login_handle.add_mo(mo, True)
-        ucs.login_handle.commit()
+            ucs.login_handle.add_mo(mo, True)
+            ucs.login_handle.commit()
 
 
 def get_ip_block(ucs, pool_dn, first_addr, last_addr, ip_version):
