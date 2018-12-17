35b481edebe67b7d9d72404b38f0368438f98ce8
“dsoper2” <dsoper@cisco.com>
2018-12-17 12:17:21 -0600

    doc updates for ucs_vlans_list

--- library/ucs_vlans_list.py
+++ library/ucs_vlans_list.py
@@ -25,52 +25,56 @@ options:
     - If C(absent), will verify VLANs are absent and will delete if needed.
     choices: [present, absent]
     default: present
-  name:
+  vlans_list:
     description:
-    - The name assigned to the VLAN.
-    - The VLAN name is case sensitive.
-    - This name can be between 1 and 32 alphanumeric characters.
-    - "You cannot use spaces or any special characters other than - (hyphen), \"_\" (underscore), : (colon), and . (period)."
-    - You cannot change this name after the VLAN is created.
-    required: yes
-  multicast_policy:
-    description:
-    - The multicast policy associated with this VLAN.
-    - This option is only valid if the Sharing Type field is set to None or Primary.
-    default: ''
-  fabric:
-    description:
-    - "The fabric configuration of the VLAN.  This can be one of the following:"
-    - "common - The VLAN applies to both fabrics and uses the same configuration parameters in both cases."
-    - "A — The VLAN only applies to fabric A."
-    - "B — The VLAN only applies to fabric B."
-    - For upstream disjoint L2 networks, Cisco recommends that you choose common to create VLANs that apply to both fabrics.
-    choices: [common, A, B]
-    default: common
-  id:
-    description:
-    - The unique string identifier assigned to the VLAN.
-    - A VLAN ID can be between '1' and '3967', or between '4048' and '4093'.
-    - You cannot create VLANs with IDs from 4030 to 4047. This range of VLAN IDs is reserved.
-    - The VLAN IDs you specify must also be supported on the switch that you are using.
-    - VLANs in the LAN cloud and FCoE VLANs in the SAN cloud must have different IDs.
-    - Optional if state is absent.
-    required: yes
-  sharing:
-    description:
-    - The Sharing Type field.
-    - "Whether this VLAN is subdivided into private or secondary VLANs. This can be one of the following:"
-    - "none - This VLAN does not have any secondary or private VLANs. This is a regular VLAN."
-    - "primary - This VLAN can have one or more secondary VLANs, as shown in the Secondary VLANs area. This VLAN is a primary VLAN in the private VLAN domain."
-    - "isolated - This is a private VLAN associated with a primary VLAN. This VLAN is an Isolated VLAN."
-    - "community - This VLAN can communicate with other ports on the same community VLAN as well as the promiscuous port. This VLAN is a Community VLAN."
-    choices: [none, primary, isolated, community]
-    default: none
-  native:
-    description:
-    - Designates the VLAN as a native VLAN.
-    choices: ['yes', 'no']
-    default: 'no'
+    - List of VLANs to configure.
+    suboptions:
+      name:
+        description:
+        - The name assigned to the VLAN.
+        - The VLAN name is case sensitive.
+        - This name can be between 1 and 32 alphanumeric characters.
+        - "You cannot use spaces or any special characters other than - (hyphen), \"_\" (underscore), : (colon), and . (period)."
+        - You cannot change this name after the VLAN is created.
+        required: yes
+      multicast_policy:
+        description:
+        - The multicast policy associated with this VLAN.
+        - This option is only valid if the Sharing Type field is set to None or Primary.
+        default: ''
+      fabric:
+        description:
+        - "The fabric configuration of the VLAN.  This can be one of the following:"
+        - "common - The VLAN applies to both fabrics and uses the same configuration parameters in both cases."
+        - "A — The VLAN only applies to fabric A."
+        - "B — The VLAN only applies to fabric B."
+        - For upstream disjoint L2 networks, Cisco recommends that you choose common to create VLANs that apply to both fabrics.
+        choices: [common, A, B]
+        default: common
+      id:
+        description:
+        - The unique string identifier assigned to the VLAN.
+        - A VLAN ID can be between '1' and '3967', or between '4048' and '4093'.
+        - You cannot create VLANs with IDs from 4030 to 4047. This range of VLAN IDs is reserved.
+        - The VLAN IDs you specify must also be supported on the switch that you are using.
+        - VLANs in the LAN cloud and FCoE VLANs in the SAN cloud must have different IDs.
+        - Optional if state is absent.
+        required: yes
+      sharing:
+        description:
+        - The Sharing Type field.
+        - "Whether this VLAN is subdivided into private or secondary VLANs. This can be one of the following:"
+        - "none - This VLAN does not have any secondary or private VLANs. This is a regular VLAN."
+        - "primary - Primary VLAN in the private VLAN domain. This VLAN can have one or more secondary VLANs, as shown in the Secondary VLANs area."
+        - "isolated - This is a private VLAN associated with a primary VLAN. This VLAN is an Isolated VLAN."
+        - "community - This VLAN can communicate with other ports on the same community VLAN as well as the promiscuous port. This VLAN is a Community VLAN."
+        choices: [none, primary, isolated, community]
+        default: none
+      native:
+        description:
+        - Designates the VLAN as a native VLAN.
+        choices: ['yes', 'no']
+        default: 'no'
 requirements:
 - ucsmsdk
 author:
@@ -80,22 +84,42 @@ version_added: '2.8'
 '''
 
 EXAMPLES = r'''
-- name: Configure VLAN
-  ucs_vlans:
+- name: Create vlans using a list
+  ucs_vlans_list:
     hostname: 172.16.143.150
     username: admin
     password: password
-    name: vlan2
-    id: '2'
-    native: 'yes'
+    vlans_list:
+    - name: vlan2
+      id: '2'
+      native: 'yes'
+    - name: vlan1000
+      id: '1000'
+    - name: vlan1001
+      id: '1001'
+    - name: vlan1002
+      id: '1002'
+    - name: vlan1003
+      id: '1003'
+    - name: vlan1004
+      id: '1004'
+    - name: vlan1005
+      id: '1005'
 
-- name: Remove VLAN
-  ucs_vlans:
+- name: Remove VLANs using a list
+  ucs_vlans_list:
     hostname: 172.16.143.150
     username: admin
     password: password
-    name: vlan2
     state: absent
+    vlans_list:
+    - name: vlan2
+    - name: vlan1000
+    - name: vlan1001
+    - name: vlan1002
+    - name: vlan1003
+    - name: vlan1004
+    - name: vlan1005
 '''
 
 RETURN = r'''
